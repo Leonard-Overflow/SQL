d036db6724bd7d92482711e992a0abedf9b2a27e
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-06-14 20:22:52 -0300

    Insert corrigido

--- dml/insert.sql
+++ dml/insert.sql
@@ -14,11 +14,13 @@ VALUES
 
 # Insert ignore
 INSERT IGNORE INTO exemplo(nome, salario)
-VALUES ("Leonardo, 5000.00");
+VALUES ("Leonardo", 5000.00);
 # Sem o ignore iria resultar em erro, pois o nome(primary key) ja foi usado
 
 # On duplicate key
 INSERT INTO exemplo(nome, salario)
 VALUE("Rafael", 6000.00)
 ON DUPLICATE KEY UPDATE salario = 6000;
-# Verifica se ha uma linha com a mesma vahve perimaria e, nesse caso, atualiza o se nao existir
+# Verifica se ha uma linha com a mesma vahve perimaria e, nesse caso, atualiza se existir
+
+SELECT * FROM exemplo
